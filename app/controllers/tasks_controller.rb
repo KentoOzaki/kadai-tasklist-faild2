644de6a660f9fdb576b76195a45c4c4fa49b87b7
kento.ozaki <love.falloutboy0412@icloud.com>
2020-01-03 13:36:56 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
+    before_action :set_task, only: [:show, :edit, :update, :destroy]
+    
     def index
         @tasks = Task.all
     end
     
     def show
-        @task = Task.find(params[:id])
+        
     end
     
     def new
@@ -24,11 +26,11 @@ class TasksController < ApplicationController
     end
     
     def edit
-        @task = Task.find(params[:id])
+        
     end
     
     def update
-        @task = Task.find(params[:id])
+        
         
         if @task.update(task_params)
             flash[:success] = "succeeded to update task"
@@ -39,7 +41,7 @@ class TasksController < ApplicationController
         end
     end
     def destroy
-        @task = Task.find(params[:id])
+        
         @task.destroy
         
         flash[:success] = "deletion was completed"
@@ -48,6 +50,11 @@ class TasksController < ApplicationController
     end
     
     private
+    
+    def set_task
+        @task = Task.find(params[:id])
+    end
+    
     def task_params
         params.require(:task).permit(:content)
     end
